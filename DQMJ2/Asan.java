ae9019ed552879dfdaab1e343d983dc99d9f1a5c
kouich <15127@hakodate.kosen-ac.jp>
2019-07-11 10:11:05 +0900

    Aがキャラ移動処理を追加

--- DQMJ2/Asan.java
+++ DQMJ2/Asan.java
@@ -14,6 +14,9 @@ public class Asan extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "up" ) ){
+        setRotation(270);
+        move(1);
+}
     }    
 }
